cb2cdacad90bbca23fd2682cc576bd20c624d4f4
TommyDIL <147648136+TommyDIL@users.noreply.github.com>
2025-03-14 14:34:31 +0100

    WIP: Wakeword recognition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use picovoice::{
+    porcupine::{Porcupine, PorcupineBuilder},
+    rhino::RhinoInference,
+    PicovoiceBuilder,
+};
+
+const ACCESS_KEY: &str = "ACCESS_KEY";
+const KEYWORD_FILE_PATH: &str = "KEYWORD_FILE_PATH";
+const MODEL_PATH: &str = "MODEL_PATH";
+
 fn main() {
+    let wake_word_callback = || {
+        // let user know wake word detected
+    };
+    let inference_callback = |inference: RhinoInference| {
+        if inference.is_understood {
+            let intent = inference.intent.unwrap();
+            let slots = inference.slots;
+            // add code to take action based on inferred intent and slot values
+        } else {
+            // add code to handle unsupported commands
+        }
+    };
+
+    let porcupine: Porcupine =
+        PorcupineBuilder::new_with_keyword_paths(ACCESS_KEY, &[KEYWORD_FILE_PATH])
+            .model_path(MODEL_PATH)
+            .init()
+            .expect("Unable to create Porcupine");
+
     println!("Hello, world!");
 }
--- target/debug/deps/info902-669a2f57893032f0.d
+++
@@ -1,5 +0,0 @@
-/var/home/tommydl/Cours/INFO902/info902/target/debug/deps/libinfo902-669a2f57893032f0.rmeta: src/main.rs
-
-/var/home/tommydl/Cours/INFO902/info902/target/debug/deps/info902-669a2f57893032f0.d: src/main.rs
-
-src/main.rs:
